e9b27ef611308ddcaac2af473a021762698e61ab
ishaankochar <ishaank25@gmail.com>
2018-01-02 19:03:08 +0530

    MonD Login setup

--- app/src/main/java/com/example/meenukochar/mond/MainActivity.java
+++ app/src/main/java/com/example/meenukochar/mond/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.meenukochar.mond;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,20 +18,40 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseUsers;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private TextView textViewName;
+    private IntentIntegrator qrScan;
+
+    private Button btn;
+
+    boolean status = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewName = (TextView) findViewById(R.id.textViewName);
+
+        qrScan = new IntentIntegrator(this);
+
+        btn = (Button) findViewById(R.id.fragment_change);
+
         mAuth = FirebaseAuth.getInstance();
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -39,6 +65,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if(!status){
+                    LeftFragment f1 = new LeftFragment();
+                    fragmentTransaction.add(R.id.FragmentContainer, f1);
+                    fragmentTransaction.commit();
+                    btn.setText("Load Second fragment");
+                    status = true;
+                }
+                else{
+                    RightFragment f2 = new RightFragment();
+                    fragmentTransaction.add(R.id.FragmentContainer, f2);
+                    fragmentTransaction.commit();
+                    btn.setText("Load First fragment");
+                    status = false;
+                }
+
+            }
+        });
+
         checkUserExist();
 
     }
@@ -64,10 +114,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    textViewName.setText(obj.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    Toast.makeText(this, "Wrong QR Code!", Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
 
         mAuth.addAuthStateListener(mAuthListener);
+        qrScan.initiateScan();
     }
 }
+
+
 
