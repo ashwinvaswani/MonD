894465e0425aba808d566ceb9f42bc139b9e734a
ishaankochar <ishaank25@gmail.com>
2018-01-26 17:00:42 +0530

    Dynamic Menu Activity and Cart Addition

---
+++ app/src/main/java/com/zconnect/mondiner/customer/models/Tabs.java
@@ -0,0 +1,46 @@
+package com.zconnect.mondiner.customer.models;
+
+/**
+ * Created by Ishaan on 20-01-2018.
+ */
+
+public class Tabs {
+
+    public Tabs(String catID, String catName, String catPriority) {
+        this.catID = catID;
+        this.catName = catName;
+        this.catPriority = catPriority;
+    }
+
+    private String catID;
+    private String catName;
+    private String catPriority;
+
+    public Tabs(){
+
+    }
+
+    public String getCatID() {
+        return catID;
+    }
+
+    public void setCatID(String catID) {
+        this.catID = catID;
+    }
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public String getCatPriority() {
+        return catPriority;
+    }
+
+    public void setCatPriority(String catPriority) {
+        this.catPriority = catPriority;
+    }
+}
